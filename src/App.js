2b2ed51578db181e1f1960c095ab42eeb8a7d48d
Calvin Dalenta <calvindalentark@gmail.com>
2021-10-04 08:12:14 +0700

    Add filter component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { ThemeProvider } from "styled-components";
+import Filter from "./components/Filter";
 import GlobalStyle from "./components/GlobalStyle";
 import Header from "./components/Header";
 import { darkTheme } from "./theme";
-
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <Header />
+      <Filter />
     </ThemeProvider>
   );
 }
