b8bf5d974021e77b2e66517699d313925c10a86b
Calvin Dalenta <calvindalentark@gmail.com>
2021-10-04 13:42:00 +0700

    Query for h2 font-size

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,12 @@ const StyledHeader = styled.div`
     background-color: ${({theme}) => theme.primary};
     padding-block: 1em;
     box-shadow: 0 2px 8px 0 rgba(0, 0, 0, 0.2);
+
+    @media screen and (max-width: 375px) {
+      h2 {
+        font-size: 1.2em;
+      }
+    }
 `;
 
 const DarkModeButton = styled.button`
