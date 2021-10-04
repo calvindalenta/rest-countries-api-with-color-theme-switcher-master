115469d6a491ea528ffdd2c023ec553e0c01d38d
Calvin Dalenta <calvindalentark@gmail.com>
2021-10-04 12:57:47 +0700

    Style details component

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,38 +1,89 @@
 import { useParams } from "react-router";
+import styled from "styled-components";
+import Container from "./Container";
+
+const StyledDetails = styled(Container)`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    align-items: center;
+    gap: 10em;
+
+    & p {
+        padding-block: 0.3em;
+
+        span {
+            font-weight: 600;
+        }
+    }
+`;
+
+const CountryDetails = styled.div`
+    display: grid;
+    grid-template-columns: 1fr;
+    gap: 2em;
+`;
+
+const Info = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+`;
+
+const BorderWrap = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 3fr;
+    align-items: center;
+`;
+
+const Borders = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    gap: 0.5em;
+`;
+
+const Border = styled.div`
+    cursor: pointer;
+    background-color: ${({theme}) => theme.primary};
+    padding: 0.3em;
+`;
 
 export default function Details({ countries }) {
     const { countryName } = useParams();
     const country = countries.find((country) => country.name === countryName);
     const currencies = country.currencies.map(currency => currency.name).join(', ');
     const languages = country.languages.map(language => language.name).join(', ');
-    const borders = country.borders.map(border => {
+    const borders = !country.borders ? '-' : country.borders.map(border => {
         const borderCountry = countries.find(country => country.alpha3Code === border);
-        return <p key={borderCountry.name}>{borderCountry.name}</p>
+        return <Border key={borderCountry.name}>{borderCountry.name}</Border>
     });
-    
+
     return (
-        <>
+        <StyledDetails>
             <img src={country.flag} alt={country.name} />
-            <h2>{country.name}</h2>
-            <div>
-                <p><span>Native Name:</span> {country.nativeName}</p>
-                <p><span>Population:</span> {country.population}</p>
-                <p><span>Region:</span> {country.region}</p>
-                <p><span>Sub Region:</span> {country.subregion}</p>
-                <p><span>Capital:</span> {country.capital}</p>
-            </div>
-            <div>
-                <p><span>Top Level Domain:</span> {country.topLevelDomain}</p>
-                <p><span>Currencies:</span> {currencies}</p>
-                <p><span>Languages:</span> {languages}</p>
-            </div>
-            <div>
-                <p>Border Countries: </p>
-                <div>
-                    {borders}
-                </div>
-            </div>
-        </>
+            <CountryDetails>
+                <h2>{country.name}</h2>
+                <Info>
+                    <div>
+                        <p><span>Native Name:</span> {country.nativeName}</p>
+                        <p><span>Population:</span> {country.population}</p>
+                        <p><span>Region:</span> {country.region}</p>
+                        <p><span>Sub Region:</span> {country.subregion}</p>
+                        <p><span>Capital:</span> {country.capital}</p>
+                    </div>
+                    <div>
+                        <p><span>Top Level Domain:</span> {country.topLevelDomain}</p>
+                        <p><span>Currencies:</span> {currencies}</p>
+                        <p><span>Languages:</span> {languages}</p>
+                    </div>
+                </Info>
+                <BorderWrap>
+                    <p><span>Border Countries:</span></p>
+                    <Borders>
+                        {borders}
+                    </Borders>
+                </BorderWrap>
+            </CountryDetails>
+        </StyledDetails>
     );
 }
 
