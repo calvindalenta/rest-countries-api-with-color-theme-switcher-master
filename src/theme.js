ba40dcaeb7ff23ac12bdf5957c5698a47ecf43c6
Calvin Dalenta <calvindalentark@gmail.com>
2021-10-04 07:04:27 +0700

    Add theme colors

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const darkTheme = {
+    primary: 'hsl(209, 23%, 22%)',
+    background: 'hsl(207, 26%, 17%)',
+    textPrimary: 'hsl(0, 0%, 100%)',
+    textSecondary: 'hsl(0, 0%, 52%)'
+}
+
+export const lightTheme = {
+    primary: 'hsl(0, 0%, 100%)',
+    background: 'hsl(0, 0%, 98%)',
+    textPrimary: 'hsl(200, 15%, 8%)',
+    textSecondary: 'hsl(0, 0%, 52%)'
+}
